ed6970046dbe225e620e73de69839e5388fe00a4
gexy5 <xinyu.ge@spacemit.com>
2023-10-16 11:49:43 +0800

    add tasks demo
    Signed-off-by: gexy5 <xinyu.ge@spacemit.com>

---
+++ demo/image_classify_demo.cc
@@ -0,0 +1,31 @@
+#include <memory>
+
+#include "task_api_factory.h"
+#include "image_classify.h"
+
+using namespace std;
+int main()
+{
+    std::unique_ptr<ImageClassify> imageclassify = TaskAPIFactory::Create<ImageClassify>();
+    imageclassify->Classify();
+    return 0;
+    /*
+    // Measure latency
+    int numTests{100};
+    std::chrono::steady_clock::time_point begin =
+        std::chrono::steady_clock::now();
+
+    //Run: Running the session is done in the Run() method:
+    for (int i = 0; i < numTests; i++) {
+        session.Run(Ort::RunOptions{nullptr}, inputNames.data(),
+                    inputTensors.data(), 1, outputNames.data(),
+                    outputTensors.data(), 1);
+    }
+    std::chrono::steady_clock::time_point end =
+        std::chrono::steady_clock::now();
+    std::cout << "Minimum Inference Latency: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / static_cast<float>(numTests)
+              << " ms" << std::endl;
+    return 0;
+    */
+}

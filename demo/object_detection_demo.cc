aaac780c3b3e74e2fc9f2ea26ae486ffbe628024
gexy5 <xinyu.ge@spacemit.com>
2023-11-02 17:05:55 +0800

    update object_detection_demo
    Signed-off-by: gexy5 <xinyu.ge@spacemit.com>

--- demo/object_detection_demo.cc
+++ demo/object_detection_demo.cc
@@ -1,31 +1,20 @@
-#include <memory>
-
+#include "cv2_utils.h"
 #include "task_api_factory.h"
 #include "object_detection.h"
 
 using namespace std;
 int main()
 {
-    std::unique_ptr<ObjectDetection> objectdetection = TaskAPIFactory::Create<ObjectDetection>();
-    objectdetection->Detect();
-    return 0;
-    /*
-    // Measure latency
-    int numTests{100};
-    std::chrono::steady_clock::time_point begin =
-        std::chrono::steady_clock::now();
+    std::string instanceName="object-detection-inference";
+    std::string modelFilepath="/home/geduer/Documents/BianbuAI/data/models/yolov4.onnx";
 
-    //Run: Running the session is done in the Run() method:
-    for (int i = 0; i < numTests; i++) {
-        session.Run(Ort::RunOptions{nullptr}, inputNames.data(),
-                    inputTensors.data(), 1, outputNames.data(),
-                    outputTensors.data(), 1);
-    }
-    std::chrono::steady_clock::time_point end =
-        std::chrono::steady_clock::now();
-    std::cout << "Minimum Inference Latency: "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / static_cast<float>(numTests)
-              << " ms" << std::endl;
+    std::string imageFilepath="/home/geduer/Documents/BianbuAI/data/imgs/4.jpg";
+    std::string save_img_path="/home/geduer/Documents/BianbuAI/data/imgs/4_result.jpg";
+    cv::Mat img_raw = cv::imread(imageFilepath);
+    std::vector<Boxf> detected_boxes;
+    std::unique_ptr<ObjectDetection> objectdetection = TaskAPIFactory::Create<ObjectDetection>();
+    detected_boxes = objectdetection->Detect(instanceName, modelFilepath, img_raw);
+    draw_boxes_inplace(img_raw , detected_boxes);
+    cv::imwrite(save_img_path, img_raw);
     return 0;
-    */
 }

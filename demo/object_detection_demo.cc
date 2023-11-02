b1ccc26a963bbe8ede883aa4047b4c57eb40ccd2
gexy5 <xinyu.ge@spacemit.com>
2023-11-02 17:37:10 +0800

    update object detection demo
    Signed-off-by: gexy5 <xinyu.ge@spacemit.com>

--- demo/object_detection_demo.cc
+++ demo/object_detection_demo.cc
@@ -7,7 +7,6 @@ int main()
 {
     std::string instanceName="object-detection-inference";
     std::string modelFilepath="/home/geduer/Documents/BianbuAI/data/models/yolov4.onnx";
-
     std::string imageFilepath="/home/geduer/Documents/BianbuAI/data/imgs/4.jpg";
     std::string save_img_path="/home/geduer/Documents/BianbuAI/data/imgs/4_result.jpg";
     cv::Mat img_raw = cv::imread(imageFilepath);
@@ -16,5 +15,7 @@ int main()
     detected_boxes = objectdetection->Detect(instanceName, modelFilepath, img_raw);
     draw_boxes_inplace(img_raw , detected_boxes);
     cv::imwrite(save_img_path, img_raw);
+    cv::imshow("detected.jpg",img_raw);
+    cv::waitKey(0);
     return 0;
 }

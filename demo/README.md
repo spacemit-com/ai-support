fee6ca1be297ac58032308fdfa346a5a3f9d6ebf
qinhongjie <hongjie.qin@spacemit.com>
2023-12-15 09:39:29 +0800

    [demo] add CmakeLists, build script and README for official release

---
+++ demo/README.md
@@ -0,0 +1,47 @@
+# User Guide
+
+## Quick Build
+
+```bash
+# Note: update the following settings if necessary
+SDK=${PATH_TO_SPACEMIT_AI_SDK} # e.g. /opt/spacemit-ai-sdk.v0.5.0
+
+# For cross compilation, try:
+CROSS_TOOL=$SDK/spacemit-gcc/bin/riscv64-unknown-linux-gnu-
+SYSROOT=$SDK/spacemit-gcc/sysroot
+BIANBUAI_HOME=$SDK/bianbu-ai-support
+ORT_HOME=$SDK/spacemit-ort
+
+# For native building, one may need to install opencv first, then try:
+#CROSS_TOOL=
+#SYSROOT=
+#BIANBUAI_HOME=$SDK/bianbu-ai-support
+#ORT_HOME=${PATH_TO_OFFICIAL_ONNXRUNTIME_RELEASE}
+
+mkdir build && pushd build
+cmake .. -DORT_HOME=${ORT_HOME} -DBIANBUAI_HOME=${BIANBUAI_HOME} -DCMAKE_C_COMPILER=${CROSS_TOOL}gcc -DCMAKE_CXX_COMPILER=${CROSS_TOOL}g++ -DCMAKE_SYSROOT=${SYSROOT}
+make -j4
+popd
+```
+
+## Quick Test
+
+```bash
+# Add qemu settings if necessary(e.g. run riscv64 demo on x86_64).
+QEMU_CMD="$SDK/qemu/bin/qemu-riscv64 -L $SYSROOT"
+# For native test, just let:
+#QEMU_CMD=
+
+# Smoke test with image classification
+env LD_LIBRARY_PATH=${ORT_HOME}/lib:$LD_LIBRARY_PATH ${QEMU_CMD} \
+  build/classification_demo data/models/squeezenet1.1-7.onnx data/labels/synset.txt data/imgs/dog.jpg
+
+# Smoke test with object detection
+env LD_LIBRARY_PATH=${ORT_HOME}/lib:$LD_LIBRARY_PATH ${QEMU_CMD} \
+  build/detection_demo data/models/nanodet-plus-m_320.onnx data/imgs/person0.jpg result0.jpg data/labels/coco.txt
+```
+
+* Model List
+
+4f22f9a64ab9612ca4372a0343b3879a  [data/models/nanodet-plus-m_320.onnx](https://bj.bcebos.com/paddlehub/fastdeploy/nanodet-plus-m_320.onnx)  
+497ad0774f4e0b59e4f2c77ae88fcdfc  [data/models/squeezenet1.1-7.onnx](https://github.com/onnx/models/blob/main/archive/vision/classification/squeezenet/model/squeezenet1.1-7.onnx)  
---
+++ demo/build.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# Author: hongjie.qin@spacemit.com
+# Brief:  Build demos and run smoke test.
+#
+
+set -e
+
+function config_native() {
+  BIANBUAI_HOME=$SDK/bianbu-ai-support
+  # Plz update the following settings !!!
+  ORT_HOME=${PATH_TO_OFFICIAL_ONNXRUNTIME_RELEASE}
+}
+
+function config_x86_riscv64() {
+  # Note: update the following settings if necessary
+  SDK=$(dirname $(which bianbu)) #$(dirname ${BASH_SOURCE[0]})
+
+  CROSS_TOOL=$SDK/spacemit-gcc/bin/riscv64-unknown-linux-gnu-
+  SYSROOT=$SDK/spacemit-gcc/sysroot
+  BIANBUAI_HOME=$SDK/bianbu-ai-support
+  ORT_HOME=$SDK/spacemit-ort
+  QEMU_CMD="$SDK/qemu/bin/qemu-riscv64 -L $SYSROOT"
+}
+
+# config
+if [[ $@ =~ "--native" ]]; then
+  config_native
+else
+  config_x86_riscv64
+fi
+
+function build() {
+  mkdir build && pushd build
+  cmake .. -DORT_HOME=${ORT_HOME} -DBIANBUAI_HOME=${BIANBUAI_HOME} -DCMAKE_C_COMPILER=${CROSS_TOOL}gcc -DCMAKE_CXX_COMPILER=${CROSS_TOOL}g++ -DCMAKE_SYSROOT=${SYSROOT}
+  make -j4
+  popd
+  echo "[INFO] Building demos done."
+}
+if [[ ! -d build ]]; then
+  build
+else
+  cd build && make && cd ..
+fi
+
+task_prepare=(
+  "if [[ ! -d data && -d ../data ]]; then ln -sf ../data .; fi"
+  "if [[ ! -d data ]]; then echo '[Error] Can not find data directory!'; exit 0; fi"
+  "mkdir -p data/models"
+  # TODO: add md5sum checking
+  "if [[ ! -f data/models/squeezenet1.1-7.onnx ]]; then wget https://media.githubusercontent.com/media/onnx/models/main/archive/vision/classification/squeezenet/model/squeezenet1.1-7.onnx -O data/models/squeezenet1.1-7.onnx; fi"
+  "if [[ ! -f data/models/nanodet-plus-m_320.onnx ]]; then wget https://bj.bcebos.com/paddlehub/fastdeploy/nanodet-plus-m_320.onnx -O data/models/nanodet-plus-m_320.onnx; fi"
+)
+task_classification=(
+  "build/classification_demo data/models/squeezenet1.1-7.onnx data/labels/synset.txt data/imgs/dog.jpg"
+)
+task_detection=(
+  "build/detection_demo data/models/nanodet-plus-m_320.onnx data/imgs/person0.jpg result0.jpg data/labels/coco.txt"
+)
+function smoke_test() {
+  # preparation(e.g. download models)
+  echo "[INFO] Prepare ..."
+  for cmd in "${task_prepare[@]}"; do eval "$cmd"; done
+  # image classification task test
+  echo "[INFO] Smoke test with image classification task ..."
+  for cmd in "${task_classification[@]}"; do
+    echo "[INFO] Run: $cmd"
+    env LD_LIBRARY_PATH=${ORT_HOME}/lib:$LD_LIBRARY_PATH ${QEMU_CMD} $cmd
+  done
+  # object detection task test
+  echo "[INFO] Smoke test with object detection task ..."
+  for cmd in "${task_detection[@]}"; do
+    echo "[INFO] Run: $cmd"
+    env LD_LIBRARY_PATH=${ORT_HOME}/lib:$LD_LIBRARY_PATH ${QEMU_CMD} $cmd
+  done
+}
+
+if [[ "$@" =~ "--test" ]]; then
+  smoke_test
+else
+  echo "[INFO] Try '${BASH_SOURCE[0]} --test' to run the demos."
+fi

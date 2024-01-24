d585ababaf10cad1a3133f6b83195809a54942d9
gexinyu <xinyu.ge@spacemit.com>
2024-01-24 17:15:19 +0800

    [demo] add preview fps
    Signed-off-by: gexinyu <xinyu.ge@spacemit.com>

--- demo/pose_tracker_stream_demo.cc
+++ demo/pose_tracker_stream_demo.cc
@@ -276,8 +276,10 @@ void Preview(DataLoader& dataloader, Tracker& tracker) {
   PoseEstimationResult poses;
   auto now = std::chrono::high_resolution_clock::now();
   poses.timestamp = now;
+  int count = 0;
+  int dur = 0;
   while (dataloader.ifenable()) {
-    // auto start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
     frame = dataloader.fetch_frame();  // 取(搬走)一帧数据
     if ((frame).empty()) {
       break;
@@ -315,20 +317,25 @@ void Preview(DataLoader& dataloader, Tracker& tracker) {
     }
     // int preview_fps = dataloader.get_preview_fps();
     int detection_fps = dataloader.get_detection_fps();
-    /*
+
     cv::putText(frame, "preview fps: " + std::to_string(preview_fps),
                 cv::Point(0, 15), cv::FONT_HERSHEY_SIMPLEX, 0.5f,
                 cv::Scalar(0, 255, 0), 1, cv::LINE_AA);
-                */
     cv::putText(frame, "detection fps: " + std::to_string(detection_fps),
                 cv::Point(500, 15), cv::FONT_HERSHEY_SIMPLEX, 0.5f,
                 cv::Scalar(0, 255, 0), 1, cv::LINE_AA);
     cv::imshow("Track", (frame));
     cv::waitKey(10);
-    // auto end = std::chrono::high_resolution_clock::now();
-    // auto preview_duration =
-    // std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    // dataloader.set_preview_fps(1000 / (preview_duration.count()));
+    auto end = std::chrono::high_resolution_clock::now();
+    auto preview_duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    count++;
+    dur = dur + preview_duration.count();
+    if (dur >= 1000) {
+      dataloader.set_preview_fps(count);
+      dur = 0;
+      count = 0;
+    }
     if (cv::getWindowProperty("Track", cv::WND_PROP_VISIBLE) < 1) {
       dataloader.set_disable();
       break;

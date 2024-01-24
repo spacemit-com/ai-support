d5e77dc7cf0e952d0123d31e03169d32d44b5884
qinhongjie <hongjie.qin@spacemit.com>
2024-01-24 20:44:38 +0800

    [src] tests: fix hard coded json test file path

--- src/tests/json_test.cc
+++ src/tests/json_test.cc
@@ -5,8 +5,12 @@
 
 using json = nlohmann::json;
 
-int main() {
-  std::ifstream f("../../../data/config/yolov6.json");
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cout << "Usage: " << argv[0] << " <json_file_path>\n";
+    return 0;
+  }
+  std::ifstream f(argv[1]);
   json config = json::parse(f);
   if (config.contains("name")) {
     std::string name = config["name"];

spacemit-com/ai-support

commit 0e205024d95fec877e05aa577f6dd59689beed10

[include&&src] code rebuild
Signed-off-by: gexinyu <xinyu.ge@spacemit.com>

src/core/engine.cc
@@ -8,15 +8,14 @@ inline std::wstring to_wstring(const std::string& input) {
 }
 #endif /* _WIN32 */
 
-int Engine::Init(std::string instanceName, std::string modelFilepath,
-                 const bool disable_spacemit_ep, const int intra_threads_num) {
+int Engine::Init(std::string instanceName, std::string modelFilepath) {
   return ortwrapper_.Init(instanceName,
 #ifdef _WIN32
                           to_wstring(modelFilepath),
 #else
-                          modelFilepath,
+                          modelFilepath
 #endif /* _WIN32 */
-                          intra_threads_num, disable_spacemit_ep);
+  );
 }
 
 int Engine::Init(json config) { return ortwrapper_.Init(config); }

include/task/vision/object_detection_task.h
@@ -11,12 +11,8 @@
 class objectDetectionTask {
  public:
   objectDetectionTask(const std::string &filePath,
-                      const std::string &labelFilepath,
-                      const bool &disable_spacemit_ep = false,
-                      const int &intra_threads_num = 4,
-                      const float &score_threshold = -1.f,
-                      const float &nms_threshold = -1.f);
-  objectDetectionTask(const std::string &filePath);
+                      const std::string &labelFilepath);
+  explicit objectDetectionTask(const std::string &filePath);
   ~objectDetectionTask() = default;
   ObjectDetectionResult Detect(const cv::Mat &raw_img);
   ObjectDetectionResult Detect(

include/task/vision/pose_estimation_task.h
@@ -10,10 +10,7 @@
 
 class poseEstimationTask {
  public:
-  poseEstimationTask(const std::string &filePath,
-                     const bool &disable_spacemit_ep = false,
-                     const int &intra_threads_num = 4);
-  poseEstimationTask(const std::string &filePath);
+  explicit poseEstimationTask(const std::string &filePath);
   ~poseEstimationTask() = default;
   PoseEstimationResult Estimate(const cv::Mat &raw_img, const Boxi &box);
 

src/core/engine.h
@@ -12,8 +12,7 @@ class Engine {
  public:
   Engine() { OrtWrapper ortwrapper_; }
   ~Engine() {}
-  int Init(std::string instanceName, std::string modelFilepath,
-           const bool disable_spacemit_ep, const int intra_threads_num);
+  int Init(std::string instanceName, std::string modelFilepath);
   int Init(json config);
   size_t GetInputCount() { return ortwrapper_.GetInputCount(); }
   size_t GetOutputCount() { return ortwrapper_.GetOutputCount(); }

src/core/ort_wrapper.cc
@@ -8,28 +8,22 @@
 #endif
 
 int OrtWrapper::Init(std::string instanceName,
-                     std::basic_string<ORTCHAR_T> modelFilepath,
-                     const int intra_threads_num,
-                     const bool disable_spacemit_ep) {
+                     std::basic_string<ORTCHAR_T> modelFilepath) {
   std::unique_ptr<Ort::Env> env(new Ort::Env(
       OrtLoggingLevel::ORT_LOGGING_LEVEL_WARNING, instanceName.c_str()));
   // Creation: The Ort::Session is created here
   env_ = std::move(env);
-  sessionOptions_.SetIntraOpNumThreads(intra_threads_num);
+  sessionOptions_.SetIntraOpNumThreads(2);
   sessionOptions_.AddConfigEntry("session.intra_op.allow_spinning", "0");
-  sessionOptions_.SetInterOpNumThreads(intra_threads_num);
+  sessionOptions_.SetInterOpNumThreads(2);
   sessionOptions_.AddConfigEntry("session.inter_op.allow_spinning", "0");
-  if (disable_spacemit_ep == true) {
-    std::cout << "Disable spacemit ep now" << std::endl;
-  } else {
 #ifdef HAS_SPACEMIT_EP
-    SessionOptionsSpaceMITEnvInit(sessionOptions_);
-    // auto providers = Ort::GetAvailableProviders();
-    std::cout << "Enable spacemit ep now" << std::endl;
+  SessionOptionsSpaceMITEnvInit(sessionOptions_);
+  // auto providers = Ort::GetAvailableProviders();
+  std::cout << "Enable spacemit ep now" << std::endl;
 #else
-    std::cout << "[Warning] Unsupport spacemit ep now" << std::endl;
+  std::cout << "Disable spacemit ep now" << std::endl;
 #endif
-  }
   // Sets graph optimization level
   // Available levels are
   // ORT_DISABLE_ALL -> To disable all optimizations

src/core/ort_wrapper.h
@@ -17,8 +17,8 @@ class OrtWrapper {
  public:
   OrtWrapper() {}
   ~OrtWrapper() {}
-  int Init(std::string instanceName, std::basic_string<ORTCHAR_T> modelFilepath,
-           const int intra_threads_num, const bool disable_spacemit_ep);
+  int Init(std::string instanceName,
+           std::basic_string<ORTCHAR_T> modelFilepath);
   int Init(json config);
   size_t GetInputCount() { return session_->GetInputCount(); }
   size_t GetOutputCount() { return session_->GetOutputCount(); }

src/task/vision/imageclassification/image_classification.cc
@@ -3,15 +3,12 @@
 #include "utils/time.h"
 
 int imageClassification::Init(const std::string modelFilepath,
-                              const std::string labelFilepath,
-                              const bool disable_spacemit_ep,
-                              const int intra_threads_num) {
+                              const std::string labelFilepath) {
   instanceName_ = "image-classification-inference";
   modelFilepath_ = modelFilepath;
   labelFilepath_ = labelFilepath;
   labels_ = readLabels(labelFilepath_);
-  initFlag_ = GetEngine()->Init(instanceName_, modelFilepath_,
-                                disable_spacemit_ep, intra_threads_num);
+  initFlag_ = GetEngine()->Init(instanceName_, modelFilepath_);
   return initFlag_;
 }
 

commit 466ad7267be2601570f6983517a666bf7827bd1a

[src] fix compile issue caused by 0e205024d(code rebuild)

src/core/engine.cc
@@ -11,7 +11,7 @@ inline std::wstring to_wstring(const std::string& input) {
 int Engine::Init(std::string instanceName, std::string modelFilepath) {
   return ortwrapper_.Init(instanceName,
 #ifdef _WIN32
-                          to_wstring(modelFilepath),
+                          to_wstring(modelFilepath)
 #else
                           modelFilepath
 #endif /* _WIN32 */
2f66a292f296be8309579eebd4bf7e3d0b70b1e9
qinhongjie <hongjie.qin@spacemit.com>
2024-07-02 19:35:40 +0800

    [python] try to support python312

--- setup.py
+++ setup.py
@@ -176,6 +176,7 @@ classifiers = [
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
 ]
 
 if __name__ == "__main__":
